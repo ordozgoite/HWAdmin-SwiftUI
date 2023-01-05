a0f812c1ac9901c4d5f07d3a043899e995f2a7f4
Victor Ordozgoite <victorordozgoite@Victors-MacBook-Air.local>
2023-01-05 10:03:21 -0400

    Firebase Integrate

---
+++ HW_Admin_SwiftUI/WebServices/WebServices.swift
@@ -0,0 +1,19 @@
+//
+//  WebServices.swift
+//  HW_Admin_SwiftUI
+//
+//  Created by Victor Ordozgoite on 05/01/23.
+//
+
+import Foundation
+
+struct WebServices {
+    
+    static var shared = WebServices()
+    
+    private init() {}
+    
+//    func getAllCars() -> [Car] {
+//        
+//    }
+}
